7f33ef3407432bb7183a656e9e463bb0d1c9c967
Ladislav Sulak <laco.sulak@gmail.com>
2022-03-07 11:31:56 +0100

    fix: adding additional requirement and fixing linting issues

--- tasks.py
+++ tasks.py
@@ -6,22 +6,41 @@ For more information, please read the official documentation:
     https://docs.getpelican.com/en/stable/index.html.
 """
 import os
+import shlex
 import shutil
 import sys
+import datetime
 
 from invoke import task
+from invoke.main import program
+from pelican import main as pelican_main
 from pelican.server import ComplexHTTPRequestHandler, RootedHTTPServer
+from pelican.settings import DEFAULT_CONFIG, get_settings_from_file
+
+# pylint: disable=unused-argument,consider-using-f-string,missing-function-docstring
+
+SETTINGS_FILE_BASE = "pelicanconf.py"
+SETTINGS = {}
+SETTINGS.update(DEFAULT_CONFIG)
+LOCAL_SETTINGS = get_settings_from_file(SETTINGS_FILE_BASE)
+SETTINGS.update(LOCAL_SETTINGS)
 
 CONFIG = {
-    # Local path configuration (can be absolute or relative to tasks.py)
-    "deploy_path": "output",
-    # Port for `serve`
+    "settings_base": SETTINGS_FILE_BASE,
+    "settings_publish": "publishconf.py",
+    # Output path. Can be absolute or relative to tasks.py. Default: 'output'
+    "deploy_path": SETTINGS["OUTPUT_PATH"],
+    # Github Pages configuration
+    "github_pages_branch": "main",
+    "commit_message": "'Publish site on {}'".format(datetime.date.today().isoformat()),
+    # Host and port for `serve`
+    "host": "localhost",
     "port": 8000,
 }
 
 
 @task
-def clean(c):  # pylint: disable=unused-argument
+def clean(c):
     """Remove generated files"""
     if os.path.isdir(CONFIG["deploy_path"]):
         shutil.rmtree(CONFIG["deploy_path"])
@@ -31,33 +50,33 @@ def clean(c):  # pylint: disable=unused-argument
 @task
 def build(c):
     """Build local version of site"""
-    c.run("pelican -s pelicanconf.py")
+    pelican_run("-s {settings_base}".format(**CONFIG))
 
 
 @task
 def rebuild(c):
     """`build` with the delete switch"""
-    c.run("pelican -d -s pelicanconf.py")
+    pelican_run("-d -s {settings_base}".format(**CONFIG))
 
 
 @task
 def regenerate(c):
     """Automatically regenerate site upon file modification"""
-    c.run("pelican -r -s pelicanconf.py")
+    pelican_run("-r -s {settings_base}".format(**CONFIG))
 
 
 @task
-def serve(c):  # pylint: disable=unused-argument
-    """Serve site at http://localhost:8000/"""
+def serve(c):
+    """Serve site at http://$HOST:$PORT/ (default is localhost:8000)"""
 
     class AddressReuseTCPServer(RootedHTTPServer):  # pylint: disable=missing-class-docstring
         allow_reuse_address = True
 
     server = AddressReuseTCPServer(
-        CONFIG["deploy_path"], ("", CONFIG["port"]), ComplexHTTPRequestHandler
+        CONFIG["deploy_path"], (CONFIG["host"], CONFIG["port"]), ComplexHTTPRequestHandler
     )
 
-    sys.stderr.write(f"Serving on port {CONFIG['port']} ...\n")
+    sys.stderr.write("Serving at {host}:{port} ...\n".format(**CONFIG))
     server.serve_forever()
 
 
@@ -71,15 +90,65 @@ def reserve(c):
 @task
 def preview(c):
     """Build production version of site"""
-    c.run("pelican -s publishconf.py")
+    pelican_run("-s {settings_publish}".format(**CONFIG))
+
+
+@task
+def livereload(c):
+    """Automatically reload browser tab upon file modification."""
+    from livereload import Server  # pylint: disable=import-outside-toplevel
+
+    def cached_build():
+        cmd = "-s {settings_base} -e CACHE_CONTENT=True LOAD_CONTENT_CACHE=True"
+        pelican_run(cmd.format(**CONFIG))
+
+    cached_build()
+    server = Server()
+    theme_path = SETTINGS["THEME"]
+    watched_globs = [
+        CONFIG["settings_base"],
+        "{}/templates/**/*.html".format(theme_path),
+    ]
+
+    content_file_extensions = [".md", ".rst"]
+    for extension in content_file_extensions:
+        content_glob = "{0}/**/*{1}".format(SETTINGS["PATH"], extension)
+        watched_globs.append(content_glob)
+
+    static_file_extensions = [".css", ".js"]
+    for extension in static_file_extensions:
+        static_file_glob = "{0}/static/**/*{1}".format(theme_path, extension)
+        watched_globs.append(static_file_glob)
+
+    for glob in watched_globs:
+        server.watch(glob, cached_build)
+    server.serve(host=CONFIG["host"], port=CONFIG["port"], root=CONFIG["deploy_path"])
 
 
 @task
 def publish(c):
     """Publish to production via rsync"""
-    c.run("pelican -s publishconf.py")
-    # pylint: disable=consider-using-f-string
+    pelican_run("-s {settings_publish}".format(**CONFIG))
     c.run(
         'rsync --delete --exclude ".DS_Store" -pthrvz -c '
-        "{} {production}:{dest_path}".format(CONFIG["deploy_path"].rstrip("/") + "/", **CONFIG)
+        '-e "ssh -p {ssh_port}" '
+        "{} {ssh_user}@{ssh_host}:{ssh_path}".format(
+            CONFIG["deploy_path"].rstrip("/") + "/", **CONFIG
+        )
     )
+
+
+@task
+def gh_pages(c):
+    """Publish to GitHub Pages"""
+    preview(c)
+    c.run(
+        "ghp-import -b {github_pages_branch} "
+        "-m {commit_message} "
+        "{deploy_path} -p".format(**CONFIG)
+    )
+
+
+def pelican_run(cmd):
+    cmd += " " + program.core.remainder  # allows to pass-through args to pelican
+    pelican_main(shlex.split(cmd))
