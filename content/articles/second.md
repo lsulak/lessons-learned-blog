e4386032af6ef61216c5f2f00acaafac9119d448
Ladislav Sulak <laco.sulak@gmail.com>
2022-04-04 15:53:02 +0200

    feat: adding CV, rewording and fron-end design improvements (#9)
    * feat: adding CV, rewording and fron-end design improvements
    
    * feat: adding additional linters
    
    * fix: linters
    
    * feat: removing unnecessary page
    
    * feat: further rewording

--- content/articles/second.md
+++
@@ -1,7 +0,0 @@
-Title: My Second Brief Article
-Date: 2022-02-26 21:12
-Category: demo
-Slug: second
-Summary: Short version for index and feeds2
-
-This is the content of my super blog post.
--- publishconf.py
+++ publishconf.py
@@ -7,11 +7,12 @@ For more information, please read the official documentation:
 """
 from __future__ import unicode_literals
 
+import os
+import sys
+
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-import os
-import sys
 
 sys.path.append(os.curdir)
 
